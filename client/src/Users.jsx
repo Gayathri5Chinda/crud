2f06dcc9ac024695cb1287c2d74ade14edbd0dbd
Gayathri5Chinda <gayathrichinda54@gmail.com>
2024-06-30 19:25:05 +0530

    commit

--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -1,16 +1,29 @@
 
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 function Users () {
-    const [users, setUsers] = useState([{
-        Name: "gaya", Email: "gayathri#gmail.com", Age:20
-    }])
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        axios.get('http://localhost:3001')
+        .then(result => setUsers(result.data))
+        .catch(err => console.log(err))
+    },[])
+
+    const handleDelete = (id) => {
+        axios.delete('http://localhost:3001/deleteUser/'+id)
+        .then(res => {console.log(res)
+            window.location.reload()})
+        .catch(errr => console.log(errr))
+    }
+
     return (
         <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
             <div className='w-50 bg-white rounded p-3'>
                 <Link to = "/create" className="btn btn-success">Add</Link>
-                <table className="table">
+                <table className = 'table'>
                     <thead>
                         <th>Name</th>
                         <th>Email</th>
@@ -21,12 +34,14 @@ function Users () {
                         {
                             users.map((user) =>{
                                 return <tr>
-                                    <td>{user.Name}</td>
-                                    <td>{user.Email}</td>
-                                    <td>{user.Age}</td>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.age}</td>
                                     <td>
-                                    <Link to = "/update" className="btn btn-success">Update</Link>
-                                        <button>Delete</button></td>
+                                       <Link to = {`/update/${user._id}`} className="btn btn-success" style={{marginRight: 30}}>Update</Link>
+                                           <button className ="btn btn-danger" 
+                                           onClick ={(e) => handleDelete(user._id)} >Delete</button>
+                                    </td>
                                 </tr>
                             })
                         }
